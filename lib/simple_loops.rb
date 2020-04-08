3689e3d18a2b92f522d427ae79fef1e8b383c888
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-08 20:19:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
 # Write your methods here
+
+def loop_message_five_times(msg)
+  5.times(puts msg)
+end
+
+def loop_message_n_times(msg,n)
+  n.times(puts msg)
+end
+
+def output_array(msg,n)
+  n.times(puts msg)
+end
+
+def return_string_array(arr)
+  str = ''
+  count=0
+  while count<arr.length do
+    str+=arr[count]
+    count++
+  end
+end
